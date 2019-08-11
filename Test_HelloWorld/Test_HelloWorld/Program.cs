0fb15da944c3c75114b138e6523618e32f46c007
Koen Bisschop <koen.bisschop@hotmail.be>
2019-08-11 19:18:36 +0200

    Eerste versie van hello world

---
+++ Test_HelloWorld/Test_HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("version 0.1.1");
+            //here comes the logic
+
+            Console.ReadLine();
+        }
+    }
+}
