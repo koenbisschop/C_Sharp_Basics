71621e20b59db75e559c2c066948fe4588a96973
Koen Bisschop <koen.bisschop@hotmail.be>
2019-08-29 11:24:28 +0200

    Geef steeds aan welke input je wenst!

--- Test_HelloWorld/Test_HelloWorld/Program.cs
+++ Test_HelloWorld/Test_HelloWorld/Program.cs
@@ -13,6 +13,7 @@ namespace Test_HelloWorld
             string naam;
             Console.WriteLine("version 0.1.1");
             //here comes the logic
+            Console.WriteLine("Geef je voornaam:");
             naam = Console.ReadLine();
             Console.WriteLine("Hello " + naam);
             Console.ReadLine();
