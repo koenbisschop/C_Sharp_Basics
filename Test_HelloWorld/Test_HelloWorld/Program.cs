0b4e6f64bae42b6a0cd94cfedebd7be69294dbc5
Koen Bisschop <koen.bisschop@hotmail.be>
2019-08-29 11:11:24 +0200

    Datum Tijd Operaties

--- Test_HelloWorld/Test_HelloWorld/Program.cs
+++ Test_HelloWorld/Test_HelloWorld/Program.cs
@@ -11,13 +11,13 @@ namespace Test_HelloWorld
         static void Main(string[] args)
         {
             string naam;
-            Int32 leeftijd;
+            DateTime geboorteDatum;
             Console.WriteLine("version 0.1.1");
             //here comes the logic
-            Console.WriteLine("Geef je leeftijd op=:");
-            leeftijd = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Geef je geboortedatum:");
+            geboorteDatum = Convert.ToDateTime(Console.ReadLine());
             naam = Console.ReadLine();
-            Console.WriteLine("Hello " + naam + "ç" + leeftijd.ToString() + ")");
+            Console.WriteLine("Hello " + naam + "(" + geboorteDatum.ToString("dddd dd/MM/yy") + ")");
             Console.ReadLine();
         }
     }
